f5fd5d8887a93329cd7ab0f13f3108a5fa85349d
Tom MacWright <tom@macwright.org>
2012-07-09 18:06:55 -0400

    First commit of mapboxjs port

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
 (function(root) {
     var Map = {},
+        MM_map,
         layers;
 
     Map = function(el, l, callback) {
-        wax.tilejson(l.api, function(t) {
-            var handlers = [
-                new MM.DragHandler(),
-                new MM.DoubleClickHandler(),
-                new MM.TouchHandler()
-            ];
-            if ($.inArray('zoomwheel', l.features) >= 0) {
-                handlers.push(new MM.MouseWheelHandler());
-            }
-            if ($.inArray('static', l.features) >= 0) {
-                handlers = null;
-            }
-
-            MM_map = new MM.Map(el, new wax.mm.connector(t), null, handlers);
-            MM_map.setCenterZoom({
+        mapbox.load(l.api, mapbox.auto(el, function(_, t) {
+            MM_map = _;
+            MM_map.centerzoom({
                 lat: (l.center) ? l.center.lat : t.center[1],
                 lon: (l.center) ? l.center.lon : t.center[0]
             }, (l.center) ? l.center.zoom : t.center[2]);
 
             if (l.zoomRange) {
                 MM_map.setZoomRange(l.zoomRange[0], l.zoomRange[1]);
-            } else {
-                MM_map.setZoomRange(t.minzoom, t.maxzoom);
             }
-
-            wax.mm.attribution(MM_map, t).appendTo(MM_map.parent);
 
             for (var i = 0; i < l.features.length; i++) {
                 switch(l.features[i]) {
-                    case 'zoompan':
-                        wax.mm.zoomer(MM_map).appendTo(MM_map.parent);
-                        break;
-                    case 'zoombox':
-                        wax.mm.zoombox(MM_map);
-                        break;
                     case 'legend':
                         MM_map.legend = wax.mm.legend(MM_map, t).appendTo(MM_map.parent);
                         break;
@@ -67,9 +46,10 @@
                         break;
                 }
             }
+
             Map.bootstrap(l);
             if (callback && typeof(callback) == 'function') callback();
-        });
+        }));
         return Map;
     };
 
@@ -86,6 +66,7 @@
         if (id) {
             if (!layers[id]) throw new Error('overlay with id ' + id + ' not found');
             var l = layers[id];
+            if (!l) return;
 
             l.group = l.group || 0;
 
@@ -95,46 +76,39 @@
                     api: l.api
                 };
             }
-        }
 
-        if ((l && l.api) || (!l && Map.layerGroups.length > 0)) {
-            var compositedLayers = function() {
-                var layerIds = [];
-                $.each(Map.layerGroups, function(index, layer) {
-                    if (layer && layer.api) {
-                        layerIds.push(layer.api.match(/v\d\/(.*?).jsonp/)[1]);
-                        $('[href="#' + layer.id + '"]').addClass('active');
+            if ((l.api) || (!l && Map.layerGroups.length > 0)) {
+                var compositedLayers = function() {
+                    var layerIds = [];
+                    $.each(Map.layerGroups, function(index, layer) {
+                        if (layer && layer.api) {
+                            layerIds.push(layer.api.match(/v\d\/(.*?).jsonp/)[1]);
+                            $('[href="#' + layer.id + '"]').addClass('active');
+                        }
+                    });
+                    return 'http://a.tiles.mapbox.com/v3/' + layerIds.join(',') + '.jsonp';
+                };
+
+                mapbox.load(compositedLayers(), function(t) {
+                    var level = (l && l.level === 'base') ? 0 : 1;
+
+                    if (MM_map.getLayers().length == 1) {
+                        MM_map.insertLayerAt(level, t.layer);
+                    } else {
+                        MM_map.setLayerAt(level, t.layer);
+                    }
+                    if (MM_map.interaction) MM_map.interaction.map(MM_map).tilejson(t);
+                    if (MM_map.legend) {
+                        MM_map.legend.content(t);
                     }
                 });
-                return 'http://a.tiles.mapbox.com/v3/' + layerIds.join(',') + '.jsonp';
-            };
+            }
 
-            wax.tilejson(compositedLayers(), function(t) {
-                var level = (l && l.level === 'base') ? 0 : 1;
-
-                try {
-                    MM_map.setLayerAt(level, new wax.mm.connector(t));
-                } catch (e) {
-                    MM_map.insertLayerAt(level, new wax.mm.connector(t));
-                }
-                if (MM_map.interaction) MM_map.interaction.map(MM_map).tilejson(t);
-                if (MM_map.legend) {
-                    MM_map.legend.content(t);
-                }
-            });
-        }
-
-        if (l && l.center) {
-            var lat = l.center.lat || MM_map.getCenter().lat,
-                lon = l.center.lon || MM_map.getCenter().lon,
-                zoom = l.center.zoom || MM_map.getZoom();
-
-            if (l.center.ease > 0) {
-                MM_map.easey = easey().map(MM_map)
-                    .to(MM_map.locationCoordinate({ lat: lat, lon: lon })
-                    .zoomTo(zoom)).run(l.center.ease);
-            } else {
-                MM_map.setCenterZoom({ lat: lat, lon: lon }, zoom);
+            if (l.center) {
+                MM_map.centerzoom({
+                    lat: (typeof l.center.lat !== 'undefined') ? l.center.lat : MM_map.getCenter().lat,
+                    lon: (typeof l.center.lat !== 'undefined') ? l.center.lon : MM_map.getCenter().lon
+                }, (typeof l.center.zoom !== 'undefined') ? l.center.zoom : MM_map.getZoom(), l.center.ease);
             }
         }
     };
@@ -172,7 +146,6 @@
             if (hash.substr(hash.length - 1, 1) === '/') hash = hash.substr(0, hash.length - 1);
 
             ids = decodeURIComponent(hash).split('/');
-
 
             $.each(ids, function(i, layer) {
                 if (layer !== '-' && layers[layer]) {
@@ -317,8 +290,8 @@ Map.bootstrap = function(l) {
         id = id.replace(/.*(?=#[^\s]+$)/, '').slice(1);
         var m = $('[data-control="geocode"]').attr('data-map') || 'main';
         e.preventDefault();
-        if($this.hasClass('active')) {
-            if(Map.layers()[id].toggle) {
+        if ($this.hasClass('active')) {
+            if (Map.layers()[id].toggle) {
                 $('[data-control="layer"]').removeClass('active');
                 window[m].removeOverlay(id);
             }
